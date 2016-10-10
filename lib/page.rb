b3defa3e05b83e1ae013e2e37ff515eace0c86f8
Oliver Denman <oliver@therewillbeanimals.com>
2016-10-10 17:18:44 +0100

    Move Page class to its own file
    While loop in scraper.rb still needs to be fixed.

---
+++ lib/page.rb
@@ -0,0 +1,31 @@
+require 'nokogiri'
+# require 'open-uri/cached'
+require 'scraped_page_archive/open-uri'
+require 'field_serializer'
+
+class String
+  def tidy
+    gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+class Page
+  include FieldSerializer
+
+  def initialize(url)
+    @url = url
+  end
+
+  def noko
+    @noko ||= Nokogiri::HTML(open(url).read)
+  end
+
+  def absolute_url(rel)
+    return if rel.to_s.empty?
+    URI.join(url, URI.encode(URI.decode(rel)))
+  end
+
+  private
+
+  attr_reader :url
+end
--- scraper.rb
+++ scraper.rb
@@ -3,37 +3,9 @@
 # frozen_string_literal: true
 
 require 'pry'
-require 'nokogiri'
-#require 'open-uri/cached'
-require 'scraped_page_archive/open-uri'
-require 'field_serializer'
+require 'require_all'
 
-class String
-  def tidy
-    gsub(/[[:space:]]+/, ' ').strip
-  end
-end
-
-class Page
-  include FieldSerializer
-
-  def initialize(url)
-    @url = url
-  end
-
-  def noko
-    @noko ||= Nokogiri::HTML(open(url).read)
-  end
-
-  def absolute_url(rel)
-    return if rel.to_s.empty?
-    URI.join(url, URI.encode(URI.decode(rel)))
-  end
-
-  private
-
-  attr_reader :url
-end
+require_rel 'lib'
 
 page = Page.new('http://house.gov.by/ru/deputies-ru/')
 
