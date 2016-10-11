3ca533a2531bc6b9f1465ae7b56ed6300236c3aa
Oliver Denman <oliver@therewillbeanimals.com>
2016-10-11 12:16:06 +0100

    Test that MemberPage returns an empty hash

--- test/member_page_test.rb
+++ test/member_page_test.rb
@@ -1,8 +1,10 @@
 require 'minitest/autorun'
 require_relative '../lib/member_page'
+require 'pry'
 
 describe 'MemberPage' do
   it 'creates a page from a url' do
     page = MemberPage.new('http://example.com')
+    page.to_h.must_equal({})
   end
 end
