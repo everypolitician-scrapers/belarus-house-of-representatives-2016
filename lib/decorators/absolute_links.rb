dbff91ac9ebadbba1c5c9439ec377627ed3c84f4
Oliver Denman <oliver@therewillbeanimals.com>
2016-11-30 12:26:42 +0000

    Add AbsoluteLinks decorator

---
+++ lib/decorators/absolute_links.rb
@@ -0,0 +1,9 @@
+class AbsoluteLinks < Scraped::Response::Decorator
+  def body
+    doc = Nokogiri::HTML(super)
+    doc.css('a').each do |link|
+      link[:href] = URI.join(url, link[:href]).to_s
+    end
+    doc.to_s
+  end
+end
