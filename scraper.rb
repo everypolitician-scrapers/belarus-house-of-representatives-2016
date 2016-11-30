398b81a95c153561f0a3c70916c6f3f06f31b18f
Oliver Denman <oliver@therewillbeanimals.com>
2016-11-30 14:11:29 +0000

    Scrape member pages
    Creates a new member page for each url in :member_urls

--- scraper.rb
+++ scraper.rb
@@ -7,9 +7,11 @@ require 'require_all'
 
 require_rel 'lib'
 
-page = MembersPage.new(
+MembersPage.new(
   response: Scraped::Request.new(
     url: 'http://house.gov.by/ru/deputies-ru/'
   )
                              .response(decorators: [AbsoluteLinks])
-)
+).member_urls.each do |member_url|
+  member_page = MemberPage.new(response: Scraped::Request.new(url: member_url).response)
+end
